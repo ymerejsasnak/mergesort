a4bfe5a200d43a6daaf53c7bb6a5cfb30bf507ec
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-11-10 13:26:44 -0500

    finished merge sort

--- mergesort.rb
+++ mergesort.rb
@@ -5,11 +5,23 @@ def mergesort(array)
   a1 = mergesort(array[0..array.size/2-1])
 	a2 = mergesort(array[array.size/2..-1])
 	
-
-
-  #temp
-	a1, a2
-
+	result = []
+	result = merge(a1, a2)
+  
+  return result
 end
 
+def merge(a1, a2)
+	result = []
+	until a1.empty? or a2.empty?
+   	if a1.first <= a2.first
+  		result << a1.first
+      a1 = a1[1..-1]
+    else
+    	result << a2.first
+    	a2 = a2[1..-1]
+    end  
+  end
+  return result + a1 + a2
+end
 
